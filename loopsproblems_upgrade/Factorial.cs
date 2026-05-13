d942b06fb73cd5070de27886986fa8cdd8f3f76a
Saurabhparaskar <saurabhparaskar28@gmail.com>
2026-05-14 02:14:22 +0530

    Added factorial program

---
+++ loopsproblems_upgrade/Factorial.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loopsproblems_upgrade
+{
+    public class Factorial
+    {
+
+        public void factorial()
+        {
+            Console.WriteLine("Enter a number to calculate its factorial:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            long result = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                result *= i;
+            }
+            Console.WriteLine($"The factorial of {number} is: {result}");
+        }
+    }
+}
