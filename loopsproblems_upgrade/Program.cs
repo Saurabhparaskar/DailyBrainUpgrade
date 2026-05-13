65024a2677deddd8db4baf7f590c10b83d47abdf
Saurabhparaskar <saurabhparaskar28@gmail.com>
2026-05-14 02:03:03 +0530

    Initial setup

---
+++ loopsproblems_upgrade/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace loopsproblems_upgrade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Loops obj = new Loops();
+            obj.PrintNumbers();
+        }
+    }
+}
---
+++ loopsproblems_upgrade/loops.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loopsproblems_upgrade
+{
+    public class Loops
+    {
+        public void PrintNumbers()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
