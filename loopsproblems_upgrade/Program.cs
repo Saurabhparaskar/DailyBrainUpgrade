2a4d1e45690ee5ee0e7f9225ea039657501e6c50
Saurabhparaskar <saurabhparaskar28@gmail.com>
2026-05-20 21:00:33 +0530

    Print Numbers using for loop Or Debuge Code..

--- loopsproblems_upgrade/Program.cs
+++ loopsproblems_upgrade/Program.cs
@@ -1,6 +1,7 @@
 ﻿namespace loopsproblems_upgrade
+ 
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
@@ -8,10 +9,13 @@
             Loops obj = new Loops();
             obj.PrintNumbers();
 
-            Factorial obj1 = new Factorial();   
+            Factorial obj1 = new Factorial();
             obj1.factorial();
             ForEachLoops f2 = new ForEachLoops();
-                f2.printArrayList();
+            f2.printArrayList();
+            PrintNumber p1 = new PrintNumber();
+            p1.PrintNumbers();
         }
     }
+
 }
