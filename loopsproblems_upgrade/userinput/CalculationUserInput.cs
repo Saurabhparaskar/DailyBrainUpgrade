2f794e4832ad5501313f290459b0c103e5ef21a5
Saurabhparaskar <saurabhparaskar28@gmail.com>
2026-06-14 07:26:49 +0530

    Implemented calculator program with user input handling

---
+++ loopsproblems_upgrade/userinput/CalculationUserInput.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace loopsproblems_upgrade.userinput
+{
+    internal class CalculationUserInput
+    {
+        int a, b, c;
+
+        public void GetUserInput()
+        {
+           
+            Console.WriteLine("Enter the first Number: ");
+            a =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second Number: ");
+            b=Convert.ToInt32(Console.ReadLine());
+
+        }
+        public void sum()
+        {
+            c = a + b;
+            Console.WriteLine("The sum of the two numbers is: " + c);
+        }
+
+
+    }
+}
